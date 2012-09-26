7b4f9342dc95ebd54eca6e0d5cb86f86ee351b46
androhi <takahiro.1828@gmail.com>
2012-09-26 17:38:08 +0900

    Add NFC logic

--- src/com/example/touchappsample/MainActivity.java
+++ src/com/example/touchappsample/MainActivity.java
@@ -1,20 +1,73 @@
 package com.example.touchappsample;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private NfcAdapter mNfcAdapter;
 
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onResume() {
+        super.onResume();
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // NFC非搭載端末
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            // NFC機能オフなので設定画面へ遷移
+            Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+            startActivity(intent);
+            return;
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()), 0);
+        IntentFilter[] intentFilters = new IntentFilter[] {
+                new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
+        };
+        String[][] techLists = {
+                {
+                        android.nfc.tech.MifareUltralight.class.getName(),
+                        android.nfc.tech.NfcA.class.getName(),
+                        android.nfc.tech.Ndef.class.getName()
+                }
+        };
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, techLists);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // ForegroundDispatch OFF
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        String action = intent.getAction();
+        if (action.equals(NfcAdapter.ACTION_TECH_DISCOVERED)) {
+            byte[] rawId = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+            Toast.makeText(this, rawId.toString(), Toast.LENGTH_SHORT);
+        }
     }
 }
